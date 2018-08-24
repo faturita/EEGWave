1946bca3e468dd416f555793f9863ab4fd9b5f4e
faturita <rramele@gmail.com>
2018-08-24 20:31:07 -0300

    Step by step chapter 2

--- Chapter2.m
+++ Chapter2.m
@@ -41,4 +41,45 @@ pause
 
 [eegimg, DOTS, zerolevel, height] = eegimageinvariant(8,output,imagescale,timescale, false,minimagesize);
 figure;imshow(eegimg);
-pause                           
+pause  
+
+%%
+output = extract(data.X, ...
+    (ceil(time1/downsize)), ...
+    floor(Fs/downsize)*windowsize);
+
+[eegimg, DOTS, zerolevel] = eegimage(8,output,imagescale,timescale, false,minimagesize);
+figure;imshow(eegimg);
+pause
+
+
+
+
+%% 
+[hdr, record] = edfread(sprintf('%s/%s/%s',getdatasetpath(),'KComplexes','excerpt1.edf'));
+Fs=200;
+plot(record(3,round(50.4113*200):round(50.4113*200+0.6544*200)))
+
+output=record(3,round(50.4113*200):round(50.4113*200+0.6544*200));
+output=output';
+[eegimg, DOTS, zerolevel] = eegimage(1,output,imagescale,timescale*2, false,minimagesize);
+
+eegimg(floor(size(eegimg,1)/2),floor(size(eegimg,2)/2)) = 0;
+
+for i=-10:10
+    eegimg(floor(size(eegimg,1)/2)+i,floor(size(eegimg,2)/2)+i) = 0;
+    eegimg(floor(size(eegimg,1)/2)+i,floor(size(eegimg,2)/2)-i) = 0;
+end
+
+
+figure;imshow(eegimg);
+
+
+% signal = record(3,:);
+% sg = bandpasseeg(signal', 1:1, Fs);
+% 
+% amplification = 12;
+% sg = zscore(sg) * amplification;
+% 
+% signal = sg;
+
