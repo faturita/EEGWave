f2385bff84d74781d076b2815e716c24a15ccfbf
faturita <rramele@gmail.com>
2018-08-31 20:02:10 -0300

    New scripts to generate all the figures

--- CalculateFigures.m
+++ CalculateFigures.m
@@ -3,17 +3,26 @@ close all
 globalrandomdelay=false;
 globalrandomamplitude=false;
 run('PerformanceBenchmark.m');
-save('performance.mat');
+save('performancesvm.mat');
 clear all
 close all
 globalrandomdelay=true;
 globalrandomamplitude=false;
 run('PerformanceBenchmark.m');
-save('performancerandomdelay.mat');
+save('performancerandomdelaysvm.mat');
 clear all
 close all
 globalrandomdelay=false;
 globalrandomamplitude=true;
 run('PerformanceBenchmark.m');
-save('performancerandomamplitude.mat');
+save('performancerandomamplitudesvm.mat');
 
+%%
+clear all
+close all
+load('performancesvm.mat');
+run('PlotPerformanceFigure.m');
+load('performancerandomdelaysvm.mat');
+run('PlotPerformanceFigure.m');
+load('performancerandomamplitudesvm.mat');
+run('PlotPerformanceFigure.m');
---
+++ PlotPerformanceFigure.m
@@ -0,0 +1,17 @@
+figure;
+set(0, 'DefaultAxesFontSize',15);
+subplot(2,5,1);plot(globalrepts1,'LineWidth',2);title('MP 1');axis([0 10 0 1.05]);
+subplot(2,5,2);plot(globalrepts2,'LineWidth',2);title('MP 2');axis([0 10 0 1.05]);
+subplot(2,5,3);plot(globalrepts3,'LineWidth',2);title('SIFT');axis([0 10 0 1.05]);
+subplot(2,5,4);plot(globalrepts6,'LineWidth',2);title('PE');axis([0 10 0 1.05]);
+subplot(2,5,5);plot(globalrepts7,'LineWidth',2);title('SHCC');axis([0 10 0 1.05]);
+subplot(2,5,6);plot(globalrepts9,'LineWidth',2);title('RS');axis([0 10 0 1.05]);
+subplot(2,5,7);plot(globalrepts10,'LineWidth',2);title('RS m-c');axis([0 10 0 1.05]);
+subplot(2,5,8);plot(globalrepts8,'LineWidth',2);title('NN s-c');axis([0 10 0 1.05]);
+subplot(2,5,9);plot(globalrepts5,'LineWidth',2);title('NN m-c');axis([0 10 0 1.05]);
+subplot(2,5,10);plot(globalrepts4,'LineWidth',2);title('SVM m-c');axis([0 10 0 1.05]);
+axis([0 10 0 1.05]);
+%ylabel('Performance')
+%set(hx,'fontSize',40);
+%set(hy,'fontSize',40);
+set(gcf, 'Position', [1, 1, 730, 329])
